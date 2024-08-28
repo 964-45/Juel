9e94261476dc98b4f4829538dbce74d5c1b3ad58
964-45 <131509219+964-45@users.noreply.github.com>
2024-08-28 14:26:05 +0530

    MY WEBSITE
    I AM VERY HAPPY

---
+++ test.js
@@ -0,0 +1,9 @@
+<a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </a>
+</div>
